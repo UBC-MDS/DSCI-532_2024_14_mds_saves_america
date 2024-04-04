a913cb91af1423f3e1a9af6f42698cf87b575f71
Tianhong Zhao <zth96@student.ubc.ca>
2024-04-03 21:22:32 -0700

    Add the age range slider

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
 import pandas as pd
 import altair as alt
+import plotly.express as px
 
 # Initialize the app
 app = Dash(__name__, external_stylesheets=['https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'])
@@ -31,6 +32,20 @@ race_ = df['race'].unique().tolist()
 ideology_ = df['ideology'].unique().tolist()
 higher_education_ = df['higher_education'].unique().tolist()
 
+# Get min and max age for the slider from the 'age' column
+min_age = df['age'].min()
+max_age = df['age'].max()
+
+# Add RangeSlider for age selection
+age_slider = dcc.RangeSlider(
+    id='age-slider',
+    min=min_age,
+    max=max_age,
+    step=1,  # step by 1 year
+    value=[min_age, max_age],  # Default slider values set to min and max ages
+    marks={i: str(i) for i in range(min_age, max_age + 1, 10)}  
+)
+
 
 # Layout
 app.layout = html.Div(
@@ -38,15 +53,14 @@ app.layout = html.Div(
     children=[
     html.H1("Cards Against Humanity: Future of Democracy and Elections in America", style={'textAlign': 'center','color': '#0660a9', 'fontSize':'24px'}),  # Add header
     html.Label('Age Range'),  # A label for the slider
-   dcc.Dropdown(
-        options=[{'label': range_val, 'value': range_val} for range_val in age_range_],  #pass the age here
-        value=age_range_[0],  # Default value
-    ),
-    html.Label('Racial Group'),
-    dcc.Dropdown(
-        options=[{'label': range_val, 'value': range_val} for range_val in race_],  #pass the race here
-        value=race_[0],  # Default value
-    ),
+    dcc.RangeSlider(
+        id='age-slider',
+        min=min_age,
+        max=max_age,
+        step=1,
+        value=[min_age, max_age],
+        marks={i: str(i) for i in range(min_age, max_age + 1, 10)}
+        ),
     html.Label('Political Ideology'),
     dcc.Dropdown(
         options=[{'label': range_val, 'value': range_val} for range_val in ideology_],  #pass the ideology here
