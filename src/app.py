76a10990de8076605ca62c73d90903957fff2ede
Sampson Yu <xiangshenyu@gmail.com>
2024-04-04 19:23:26 -0700

    added war likelihood bar chart and refined the layout

--- src/app.py
+++ src/app.py
@@ -3,68 +3,316 @@ import dash_bootstrap_components as dbc
 import dash_vega_components as dvc
 import pandas as pd
 import altair as alt
+import plotly.express as px
 
 # Initialize the app
-app = Dash(__name__, external_stylesheets=['https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'])
+app = Dash(__name__,
+           external_stylesheets=['https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'])
 
-# Sample data (replace this with your actual data loading logic)
+colors = {'light_blue': '#0d76bd',
+          'dark_blue': '#0660a9',
+          'red': '#ed1c23',
+          'white': '#ffffff',
+          'grey': '#888888',
+          'light_grey': '#d3d3d3'
+          }
+
 df = pd.read_csv("data/processed/data_cleaned.csv")
 
-
-# Charts 
-# Calculate percentages for each political party within each race
-df_pct = df.groupby(['race', 'political_party']).size().unstack(fill_value=0).apply(lambda x: x / x.sum(), axis=1).stack().reset_index(name='percentage')
-
-# Create Altair chart
-race_pol_party_chart1 = alt.Chart(df_pct).mark_bar().encode(
-    x=alt.X('race', axis=alt.Axis(title='Race')),
-    y=alt.Y('percentage:Q', axis=alt.Axis(format='%'), stack='normalize', title='Percentage'),
-    color=alt.Color('political_party', legend=alt.Legend(title='Political Party'))
-).properties(
-    title='100% Stacked Bar Chart of Political Party by Race'
-)
-
-
-# Get unique age range values for drop downs
 age_range_ = df['age_range'].unique().tolist()
 race_ = df['race'].unique().tolist()
 ideology_ = df['ideology'].unique().tolist()
 higher_education_ = df['higher_education'].unique().tolist()
 
+min_age = df['age'].min()
+max_age = df['age'].max()
 
-# Layout
-app.layout = html.Div(
-    style={'backgroundColor': '#A6CAEC'},  # Change background color here
-    children=[
-    html.H1("Cards Against Humanity: Future of Democracy and Elections in America", style={'textAlign': 'center','color': '#0660a9', 'fontSize':'24px'}),  # Add header
-    html.Label('Age Range'),  # A label for the slider
-   dcc.Dropdown(
-        options=[{'label': range_val, 'value': range_val} for range_val in age_range_],  #pass the age here
-        value=age_range_[0],  # Default value
-    ),
-    html.Label('Racial Group'),
-    dcc.Dropdown(
-        options=[{'label': range_val, 'value': range_val} for range_val in race_],  #pass the race here
-        value=race_[0],  # Default value
-    ),
-    html.Label('Political Ideology'),
-    dcc.Dropdown(
-        options=[{'label': range_val, 'value': range_val} for range_val in ideology_],  #pass the ideology here
-        value=ideology_[0],  # Default value
-    ),
-    html.Label('Level of Higher Education'),
-    dcc.Dropdown(
-        options=[{'label': range_val, 'value': range_val} for range_val in higher_education_],  #pass the higher_education here
-        value=higher_education_[0],  # Default value
-    ),
-    html.Br(),  # Add whitespace; br = "break",
-    html.Br(),  # Add whitespace; br = "break",
-    dvc.Vega(spec=race_pol_party_chart1.to_dict())
-])
+df_pct = df.groupby(['race', 'political_party']).size().unstack(fill_value=0).apply(
+    lambda x: x / x.sum(), axis=1).stack().reset_index(name='percentage')
+df_pct_education = df.groupby(['higher_education', 'political_party']).size().unstack(
+    fill_value=0).apply(lambda x: x / x.sum(), axis=1).stack().reset_index(name='percentage')
 
-# Server side callbacks/reactivity
-# ...
 
-# Run the app/dashboard
+race_pol_party_chart1 = alt.Chart(df_pct).mark_bar().encode(
+    x=alt.X('race', axis=alt.Axis(title='Race')),
+    y=alt.Y('percentage:Q', axis=alt.Axis(format='%'),
+            stack='normalize', title='Percentage'),
+    color=alt.Color('political_party', legend=alt.Legend(
+        title='Political Party'))
+).properties(
+    title='100% Stacked Bar Chart of Political Party by Race'
+)
+
+
+def create_stacked_chart_race(df):
+    party_colors = {'Republican': colors['red'],
+                    'Democrat': colors['dark_blue'],
+                    'DK/REF': colors['white'],
+                    'Independent': colors['light_blue']}
+
+    fig = px.bar(df, x='race', y='percentage', color='political_party',
+                 title='Stacked Bar Chart of Political Party by Race',
+                 labels={'percentage': 'Percentage', 'race': 'Race'},
+                 category_orders={'race': sorted(df['race'].unique())},
+                 hover_data={'percentage': ':.2%'},
+                 barmode='relative',
+                 color_discrete_map=party_colors)
+
+    fig.update_yaxes(title='Percentage', tickformat='%')
+    fig.update_layout(
+        {
+            "paper_bgcolor": colors['light_grey'],
+            "plot_bgcolor": colors['light_grey'],
+        },
+        legend_title='Political Party',
+    )
+
+    return fig
+
+
+def create_stacked_chart_education(df):
+    party_colors = {'Republican': colors['red'],
+                    'Democrat': colors['dark_blue'],
+                    'DK/REF': colors['white'],
+                    'Independent': colors['light_blue']}
+
+    fig = px.bar(df, x='higher_education', y='percentage', color='political_party',
+                 title='Stacked Bar Chart of Political Party by Highest Education',
+                 labels={'percentage': 'Percentage',
+                         'higher_education': 'Higher Education'},
+                 category_orders={'higher_education': sorted(
+                     df['higher_education'].unique())},
+                 hover_data={'percentage': ':.2%'},
+                 barmode='relative',
+                 color_discrete_map=party_colors)
+
+    fig.update_yaxes(title='Percentage', tickformat='%')
+    fig.update_layout(
+        {
+            "paper_bgcolor": colors['light_grey'],
+            "plot_bgcolor": colors['light_grey'],
+        },
+        legend_title='Political Party',
+    )
+
+    return fig
+
+
+def create_donut_chart(df):
+    # Calculate the counts for each category in the 'trump_2020' column
+    counts = df['trump_2020'].value_counts().reset_index()
+    counts.columns = ['trump_2020', 'counts']
+
+    # Create the pie chart with a hole (donut chart)
+    fig_donut = px.pie(counts, values='counts', names='trump_2020', hole=0.4)
+
+    # Update the layout and trace for the donut chart appearance
+    fig_donut.update_traces(textinfo='percent+label', pull=[0.1 if i == 0 else 0 for i in range(len(counts))],
+                            marker=dict(colors=list(colors.values())[:len(df['trump_2020'].unique())]))
+    fig_donut.update_layout(
+        {
+            "paper_bgcolor": colors['light_grey'],
+            "plot_bgcolor": colors['light_grey'],
+        },
+        title='Likelihood of Donald Trump Reelection in 2020',
+        showlegend=True,
+        annotations=[dict(text='Trump', x=0.5, y=0.5,
+                          font_size=20, showarrow=False)],
+        plot_bgcolor=colors['light_grey'],
+
+
+
+    )
+    return fig_donut
+
+
+def create_war_likelihood_chart(df):
+    # Define colors for the opinions to match the American flag theme
+    opinion_colors = {'Very Likely': colors['red'],
+                      'Somewhat Likely': colors['white'],
+                      'Not at all likely': colors['dark_blue']}
+
+    # Map the numeric values to string categories if they represent categorical data
+    likelihood_mapping = {0: 'Not at all likely',
+                          1: 'Somewhat Likely', 2: 'Very Likely'}
+    df['likelihood_of_war'] = df['likelihood_of_war'].map(likelihood_mapping)
+
+    # Aggregate the data to get the count of responses for each category
+    likelihood_counts = df['likelihood_of_war'].value_counts().reset_index()
+    likelihood_counts.columns = ['Opinion', 'Count']
+
+    # Sort the dataframe based on the opinion order you want in the chart
+    ordered_opinions = ['Very Likely', 'Somewhat Likely', 'Not at all likely']
+    likelihood_counts['Opinion'] = pd.Categorical(
+        likelihood_counts['Opinion'], categories=ordered_opinions, ordered=True)
+    likelihood_counts = likelihood_counts.sort_values('Opinion')
+
+    # Create the horizontal bar chart using Plotly Express
+    fig = px.bar(likelihood_counts, y='Opinion', x='Count',
+                 title='Perceived Likelihood of War',
+                 labels={'Count': 'Number of Responses',
+                         'Opinion': 'Opinion on Likelihood of War'},
+                 text='Count', orientation='h',
+                 color='Opinion',
+                 color_discrete_map=opinion_colors)
+
+    fig.update_layout(
+        {
+            "paper_bgcolor": colors['light_grey'],
+            "plot_bgcolor": colors['light_grey'],
+        }
+    )
+    return fig
+
+ # Use the function to create the figure
+donut_chart_figure = create_donut_chart(df)
+stacked_chart_race = create_stacked_chart_race(df_pct)
+stacked_chart_education = create_stacked_chart_education(df_pct_education)
+war_likelihood_chart = create_war_likelihood_chart(df)
+
+# Donut chart component
+donut_chart_component = dcc.Graph(
+    id='donut-chart',
+    figure=donut_chart_figure
+)
+
+stacked_chart_component_race = dcc.Graph(
+    id='stacked-chart-race',
+    figure=stacked_chart_race
+)
+
+stacked_chart_component_education = dcc.Graph(
+    id='stacked-chart-education',
+    figure=stacked_chart_education
+)
+
+war_likelihood_chart_component = dcc.Graph(
+    id='war-likelihood-chart',
+    figure=war_likelihood_chart
+)
+
+
+app.layout = html.Div([
+    html.Link(
+        href='https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/css/bootstrap.min.css',
+        rel='stylesheet'
+    ),
+    # Header Row with title and filters
+    dbc.Row([
+        dbc.Col(html.H1("Cards Against Humanity: Future of Democracy and Elections in America",
+                        style={'textAlign': 'center', 'color': '#0660a9', 'fontSize': '24px'})),
+    ]),
+
+    # Filters row
+    dbc.Row([
+        dbc.Col(html.Label("Age Range", htmlFor="age-slider",
+                style={'color': colors['white']}), width=1),
+        dbc.Col(dcc.RangeSlider(id='age-slider', min=min_age, max=max_age, step=1, value=[min_age, max_age],
+                                marks={i: str(i) for i in range(min_age, max_age + 1, 10)}), width=10),
+    ], style={'backgroundColor': colors['light_blue'], 'padding': '10px'}),
+
+    dbc.Row([
+        dbc.Col(html.Label("Racial Group", htmlFor="racial-group-dropdown",
+                style={'color': colors['white']}), width=1),
+        dbc.Col(dcc.Dropdown(options=[{'label': range_val, 'value': range_val} for range_val in race_],
+                             value=race_[0], id="racial-group-dropdown"), width=2),
+        dbc.Col(html.Label("Political Ideology", htmlFor="ideology-dropdown",
+                style={'color': colors['white']}), width=1),
+        dbc.Col(dcc.Dropdown(options=[{'label': range_val, 'value': range_val} for range_val in ideology_],
+                             value=ideology_[0], id="ideology-dropdown"), width=2),
+        dbc.Col(html.Label("Level of Higher Education",
+                htmlFor="higher-education-dropdown", style={'color': colors['white']}), width=2),
+        dbc.Col(dcc.Dropdown(options=[{'label': range_val, 'value': range_val} for range_val in higher_education_],
+                             value=higher_education_[0], id="higher-education-dropdown"), width=2),
+    ], style={'backgroundColor': colors['light_blue'], 'padding': '10px'}),
+
+
+    # Main content row with two columns for the two main sections
+    dbc.Row([
+        # Column for "General Correlations" with both charts side by side
+        dbc.Col([
+            html.H2("General Correlations", style={'textAlign': 'center'}),
+            dbc.Row([
+                dbc.Col(dcc.Graph(id='stacked-chart-race',
+                        figure=stacked_chart_race), md=6),
+                dbc.Col(dcc.Graph(id='stacked-chart-education',
+                        figure=stacked_chart_education), md=6),
+            ]),
+            dbc.Row(war_likelihood_chart_component),
+        ], md=6),
+
+        # Column for "Elections: Donald Trump Focused"
+        dbc.Col([
+            html.H2("Elections: Donald Trump Focused",
+                    style={'textAlign': 'center'}),
+            dbc.Row(dcc.Graph(id='donut-chart', figure=donut_chart_figure)),
+        ], md=6),
+    ], style={'marginTop': 30}),
+], style={'backgroundColor': colors['light_grey']})
+
+
+@app.callback(
+    Output('donut-chart', 'figure'),
+    [Input('age-slider', 'value'),
+     Input('higher-education-dropdown', 'value'),
+     Input('ideology-dropdown', 'value'),
+     Input('racial-group-dropdown', 'value')]
+)
+def update_donut_chart(age_range,
+                       education,
+                       ideology,
+                       race):
+    filtered_df = df[(df['age'] >= age_range[0]) & (df['age'] <= age_range[1])]
+    filtered_df = filtered_df[filtered_df['higher_education'] == education]
+    filtered_df = filtered_df[filtered_df['ideology'] == ideology]
+    filtered_df = filtered_df[filtered_df['race'] == race]
+
+    return create_donut_chart(filtered_df)
+
+
+@app.callback(
+    Output('stacked-chart-race', 'figure'),
+    [Input('age-slider', 'value'),
+     Input('higher-education-dropdown', 'value'),
+     Input('ideology-dropdown', 'value'),
+     Input('racial-group-dropdown', 'value')]
+)
+def update_stacked_chart_race(age_range,
+                              education,
+                              ideology,
+                              race):
+    filtered_df = df[(df['age'] >= age_range[0]) & (df['age'] <= age_range[1])]
+    filtered_df = filtered_df[filtered_df['higher_education'] == education]
+    filtered_df = filtered_df[filtered_df['ideology'] == ideology]
+    filtered_df = filtered_df[filtered_df['race'] == race]
+
+    df_pct = filtered_df.groupby(['race', 'political_party']).size().unstack(
+        fill_value=0).apply(lambda x: x / x.sum(), axis=1).stack().reset_index(name='percentage')
+
+    return create_stacked_chart_race(df_pct)
+
+
+@app.callback(
+    Output('stacked-chart-education', 'figure'),
+    [Input('age-slider', 'value'),
+     Input('higher-education-dropdown', 'value'),
+     Input('ideology-dropdown', 'value'),
+     Input('racial-group-dropdown', 'value')]
+)
+def update_stacked_chart_education(age_range,
+                                   education,
+                                   ideology,
+                                   race):
+    filtered_df = df[(df['age'] >= age_range[0]) & (df['age'] <= age_range[1])]
+    filtered_df = filtered_df[filtered_df['higher_education'] == education]
+    filtered_df = filtered_df[filtered_df['ideology'] == ideology]
+    filtered_df = filtered_df[filtered_df['race'] == race]
+
+    df_pct = filtered_df.groupby(['higher_education', 'political_party']).size().unstack(
+        fill_value=0).apply(lambda x: x / x.sum(), axis=1).stack().reset_index(name='percentage')
+
+    return create_stacked_chart_education(df_pct)
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
